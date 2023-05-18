54931fa656924ce0b2d5d5ae9325019598112069
github_username <email_address>
2023-05-18 19:44:26 +0300

    fix linter errors

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 import { projectsData } from './data.js';
 
-// console.log(projectData);
+console.log(projectData);
 
 for (let i = 0; i < projectsData.length; i++) {
   const element = projectsData[i];
